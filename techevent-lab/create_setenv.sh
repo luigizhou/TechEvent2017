dacb69e1ff8542715f2b4f55f9703873e6dadb65
Zhou Luigi <luigi.zhou@ynap.com>
2017-03-16 11:39:22 +0100

    create setenv.sh

---
+++ techevent-lab/create_setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n "Enter your AWS ACCESS KEY and press [ENTER]: "
+read awsaccesskey
+echo -n "Enter your AWS SECRET ACCESS KEY and press [ENTER]: "
+read secretaccesskey
+
+echo "#!/bin/bash" >> setenv.sh
+echo "export AWS_ACCESS_KEY_ID=\"${awsaccesskey}\"" >> setenv.sh
+echo "export AWS_SECRET_ACCESS_KEY=\"${secretaccesskey}\"" >> setenv.sh
