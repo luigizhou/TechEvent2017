c12a04baf3ba29cb28d39eb799cd302bfa3fb6dd
Zhou Luigi <luigi.zhou@ynap.com>
2017-03-16 11:25:25 +0100

    renamed lab folder

--- terraform_aws/userdata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-sudo su
-sudo yum update -y 
-sudo yum install httpd -y
-sudo service httpd start
-sudo service httpd enable
-echo '<h1>' $(hostname) '</h1>' >> /var/www/html/index.html
